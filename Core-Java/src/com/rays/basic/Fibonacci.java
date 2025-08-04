18614d5ff4b4c2ec5a95c6624c12a1b4d20983ad
krati2709 <kratitrivedi27@gmail.com>
2025-08-04 15:50:06 +0530

    new

---
+++ Core-Java/src/com/rays/basic/Fibonacci.java
@@ -0,0 +1,19 @@
+package com.rays.basic;
+
+import java.util.*;
+public class Fibonacci {
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter number: ");
+	int i = sc.nextInt();
+	
+	while(i<1000) {
+	
+	System.out.println(i);
+	i = i+i;
+	}
+	
+}
+}
+
