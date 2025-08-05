dd53e3ecb5242f5102984cbcce3b42a8d62b8c2a
krati2709 <kratitrivedi27@gmail.com>
2025-08-05 11:38:05 +0530

    new

--- Core-Java/src/com/rays/basic/FindNoInArray.java
+++ Core-Java/src/com/rays/basic/FindNoInArray.java
@@ -3,14 +3,18 @@ package com.rays.basic;
 public class FindNoInArray {
 	public static void main(String[] args) {
 		int[] a = {83, 93, 65, 92, 43, 92};
-		int n = 92;
-		int position = 0;
+		int n = 5;
+		int position = -1;
 		
 		for (int i = 0; i < a.length; i++) {
-			position = 1;
 			if (a[i]==n) {
-				System.out.println("postion of 92 is: " + i );
+				position = i;
+				System.out.println("position: " + i );
 			}
+		}
+		if (position == -1) {
+			System.out.println("position: " + position);
+			
 		}
 	}
 }
