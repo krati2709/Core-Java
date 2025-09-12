06d6e6eb1697e00e088804b062d28be01a65fb2f
krati2709 <kratitrivedi27@gmail.com>
2025-09-12 17:37:44 +0530

    new

--- Java-Thread/src/com/rays/thread/TestIrctc.java
+++ Java-Thread/src/com/rays/thread/TestIrctc.java
@@ -5,7 +5,7 @@ import com.rays.thread.irctc.IrctcThread;
 public class TestIrctc {
 	public static void main(String[] args) {
 		
-		IrctcThread it1 = new IrctcThread("ram", 5);
+		IrctcThread it1 = new IrctcThread("ram", 2);
 		IrctcThread it2 = new IrctcThread("shyam", 4);
 		
 		it1.start();
--- Java-Thread/src/com/rays/thread/irctc/Irctc.java
+++ Java-Thread/src/com/rays/thread/irctc/Irctc.java
@@ -6,14 +6,21 @@ import java.util.Set;
 
 public class Irctc {
 
-	private Set<Integer> bookedTickets = new HashSet<>();
+	Set<Integer> bookedTickets = new HashSet<>();
 	Random r = new Random();
 
 	public synchronized void bookTicket(String name, int num) {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		int ticket;
 		do {
 			ticket = r.nextInt(72) + 1;
 		} while (bookedTickets.contains(ticket));
+		bookedTickets.add(ticket);
 		System.out.println(name + ": " + ticket);
 	}
 }
